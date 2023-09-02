305bbd4dccd09ea6fc91f07bf560f5608b1cff93
Haythem <121164730+haythem297@users.noreply.github.com>
2023-09-02 16:23:49 +0200

    Add files via upload

--- doctor_dashboard.sql
+++ doctor_dashboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 01 sep. 2023 à 21:10
+-- Généré le : sam. 02 sep. 2023 à 16:21
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -38,7 +38,8 @@ CREATE TABLE `doctors` (
 --
 
 INSERT INTO `doctors` (`id`, `username`, `password`) VALUES
-(1, 'haythem', 'haythem123');
+(1, 'haythem', 'haythem123'),
+(2, 'marwa', 'sankou7');
 
 -- --------------------------------------------------------
 
@@ -50,18 +51,22 @@ CREATE TABLE `patients` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `phone_number` varchar(20) DEFAULT NULL
+  `phone_number` varchar(20) DEFAULT NULL,
+  `rdv` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `patients`
 --
 
-INSERT INTO `patients` (`id`, `name`, `email`, `phone_number`) VALUES
-(2, 'hamza', 'hamza123@gmail.com', '14578487'),
-(4, 'daffouli', 'daffouli123@gmail.com', '45124787'),
-(5, 'ahmed', 'ahmed@gmail.com', '47874787'),
-(6, 'sihem', 'sihem@gmail.com', '45741245');
+INSERT INTO `patients` (`id`, `name`, `email`, `phone_number`, `rdv`) VALUES
+(2, 'hamza', 'hamza123@gmail.com', '14578487', '2023-09-02'),
+(4, 'daffouli', 'daffouli123@gmail.com', '45124787', '2023-09-02'),
+(5, 'ahmed', 'ahmed@gmail.com', '47874787', '2023-09-02'),
+(6, 'sihem', 'sihem@gmail.com', '45741245', '2023-09-02'),
+(7, 'wajdi sadaoui', 'wajdi@gmail.com', '457874124', '2023-09-02'),
+(8, 'ahmed rkiik', 'rkiik@gmail.com', '45784124', '2023-09-02'),
+(9, 'manel', 'manel@gmail.com', '47874214', '2023-09-15');
 
 --
 -- Index pour les tables déchargées
@@ -87,13 +92,13 @@ ALTER TABLE `patients`
 -- AUTO_INCREMENT pour la table `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT pour la table `patients`
 --
 ALTER TABLE `patients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
